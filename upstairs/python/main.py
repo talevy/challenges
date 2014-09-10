a499085888689f389ef36e7d61a30acba20db2ad
tal <tal@Miris-MacBook-Air.local>
2014-09-10 16:04:29 -0700

    add slow python solution

--- upstairs/python/main.py
+++ upstairs/python/main.py
@@ -1,3 +1,31 @@
 import sys
 
-print sum(map(int, sys.stdin.next().strip().split(' ')))
+def num_steps_climbed(floor_schedule):
+    return sum(map(
+	    lambda (x, y): y-x if y > x else 0,
+	    zip(floor_schedule, floor_schedule[1:])))
+
+def swap(i, floor_schedule):
+	tmp = floor_schedule[i]
+	floor_schedule[i] = floor_schedule[i+1]
+	floor_schedule[i+1] = tmp
+
+stdin = sys.stdin
+
+N = int(stdin.next().strip())
+floor_schedule = map(int, stdin.next().strip().split())
+
+# Solve
+
+min_steps = num_steps_climbed(floor_schedule)
+f = -1
+
+for i, a in enumerate(floor_schedule[:-1]):
+    swap(i, floor_schedule)
+    next_min = num_steps_climbed(floor_schedule)
+    if next_min < min_steps:
+        min_steps = next_min
+	f = i + 1
+    swap(i, floor_schedule)
+ 
+print f
